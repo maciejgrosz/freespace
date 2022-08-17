80fe9c6b2a63b8affa90a2468dd1e462fa38025c
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-08-17 09:36:11 +0200

    delete unnecessery part (legacy part from old recursive method

--- freespace.sh
+++ freespace.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 usage() {
   cat << EOF >&2
 Usage: $0 [-r] [-t ###] file [file...]
@@ -19,8 +21,8 @@ popd () {
 }
 
 function addSuffix() {
-    DIR=$(dirname "$1");
-    FILENAME=$(basename "$1");
+    local DIR=$(dirname "$1");
+    local FILENAME=$(basename "$1");
     NEWNAME=$DIR/fc-$FILENAME
     mv "$1" "$NEWNAME"
 
@@ -38,7 +40,7 @@ function changeZipName() {
 }
 
 function processZipFiles(){
-    BASE=$(basename "$1")
+    local BASE=$(basename "$1")
     if [[ $BASE == "fc-"* ]]; then 
         if [ $(stat --format=%Y "$1") -le $(( $(date +%s) - $TIMEOUT*3600 )) ]; then
             rm "$1"
@@ -48,23 +50,24 @@ function processZipFiles(){
     fi    
 }
 function freespace() {
-    for FILE in "$@"; do
-        if [[ -d "$FILE" ]]; then
-            continue
-        fi
-        file_type=$(file -b "$FILE" | awk '{print $1}')
-        case $file_type in 
-            *gzip|*Zip|*bzip2|compress*) processZipFiles "$FILE";; 
-            *) compressFile "$FILE";
-        esac
-    done
+    # for FILE in "$@"; do #its not necessery with "find" method
+    # if [[ -d "$1" ]]; then
+    #     continue
+    # fi
+    file_type=$(file -b "$1" | awk '{print $1}')
+    case $file_type in 
+        *gzip|*Zip|*bzip2|compress*) processZipFiles "$1";; 
+        *) compressFile "$1";
+    esac
+    # done
 }
 
 function manageFreespace() {
+    local listOfFiles
     if [ "$RECURSIVE" == "-r" ]; then
-        listOfFiles=$(find $@ -name "*")
+        listOfFiles=$(find "$@" -name "*")
     else
-        listOfFiles=$(ls $@)
+        listOfFiles=$(find "$@" -maxdepth 1 -name "*")
     fi
     for FILE in $listOfFiles; do
         if [[ -f "$FILE" ]]; then
@@ -72,7 +75,8 @@ function manageFreespace() {
         fi
     done
 }
-# function manageFreespace(){
+
+# function manageFreespace(){  #Different way to do recursive
 #     for FILE in "$@"; do
 #         if [[ -f "$FILE" ]]; then
 #             freespace "$FILE"
@@ -107,11 +111,9 @@ function main() {
         esac
     done    
     shift $((OPTIND -1))
-
     manageFreespace "$@"
     local return_value=$?
     return $return_value
-
 }
 
-main "$@"
+main "$@" 
